131a41c5082ce83d40c6c812c0a2574bb1d8685a
0308bae <69972996+0308bae@users.noreply.github.com>
2021-05-20 17:51:19 +0900

    Delete SavingAccount.h

--- SavingAccount.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include "Account.h"
-
-class SavingAccount :public Account {
-public:
-	virtual void deposit() final;
-	virtual std::string getAccountType() final;
-};
