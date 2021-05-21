ac9e9f5fcefaed0038670b7dd8e06c86c83f1c9c
0308bae <69972996+0308bae@users.noreply.github.com>
2021-05-21 17:27:59 +0900

    Add files via upload

--- opp_team10_2021/Account.h
+++ opp_team10_2021/Account.h
@@ -1,19 +1,23 @@
 #pragma once
+#define LOAN_RATE 1.5
+#define DEPOSIT_RATE 0.1
 #include <string>
 #include <utility>
 
 class Account {
-    unsigned int mAccNum;
+    std::string mAccNum;
     unsigned int mBalance;
     std::string mPerName;
     std::string mBnkName;
     std::pair<int, int> mLoanInfo;
-    int mPwd;
+    unsigned int mPwd;
 public:
+    Account(std::string accNum, unsigned balance, std::string perName, std::string bnkName, unsigned int pwd);
+    Account(std::string perName, std::string mBnkName, unsigned int pwd);
     virtual std::string getAccountType();
     virtual void deposit();
     void printAccount();
-    unsigned int getAccNum();
+    std::string getAccNum();
     unsigned int getBalance();
     std::string getPerName();
     std::string getBnkName();
